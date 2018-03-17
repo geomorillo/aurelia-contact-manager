040d68e324245cd7325db937ea775fa89333d853
Lance Newman <lancelot316@msn.com>
2018-03-17 15:06:53 -0400

    Adding A Loading Indicator

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import { Router, RouterConfiguration } from 'aurelia-router';
-import { PLATFORM } from 'aurelia-pal';
+import { inject, PLATFORM } from 'aurelia-framework';
+import { WebAPI } from './web-api';
 
+@inject(WebAPI)
 export class App {
   router: Router;
+
+  constructor(public api: WebAPI) { }
 
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Contacts';
     config.map([
-      { route: '',              moduleId: PLATFORM.moduleName('no-selection'),   title: 'Select' },
-      { route: 'contacts/:id',  moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
+      { route: '',             moduleId: PLATFORM.moduleName('no-selection'), title: 'Select' },
+      { route: 'contacts/:id', moduleId: PLATFORM.moduleName('contact-detail'), name: 'contacts' }
     ]);
 
     this.router = router;
